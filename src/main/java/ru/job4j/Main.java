7e8a2db39073ff677d8ef890e6e99b6158d6de0e
F-Alexey <f.alexey.workspace@gmail.com>
2023-10-30 00:07:35 +0300

    modified calc in Main

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -6,5 +6,7 @@ public class Main {
         Calculator.main();
         System.out.println("А сейчас запустим его же но с вводом изнутри");
         Calculator.autoCalc(1,3,"+");
+        int twise = 4;
+        System.out.println("квадрат 4х равен" + Math.pow(twise, 2));
     }
 }
