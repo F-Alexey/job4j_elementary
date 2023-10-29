70cb8c76fd7ef9ca86cd9bcec59ec553aa1f00ed
F-Alexey <f.alexey.workspace@gmail.com>
2023-10-29 22:01:42 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args){
+        System.out.println("Alexey Fedoseev");
+        System.out.println("15.03.1995");
+    }
+}
