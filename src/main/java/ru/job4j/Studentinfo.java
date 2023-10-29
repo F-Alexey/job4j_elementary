1758fe2912af829e5ae55c935b583634dabf6eb0
F-Alexey <f.alexey.workspace@gmail.com>
2023-10-29 22:29:51 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Studentinfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Alexey Fedoseev ");
         System.out.println("15.03.1995");
     }
