7d39e0e19f4701a84703c4ab287e8bba0ab1bb60
F-Alexey <f.alexey.workspace@gmail.com>
2023-10-29 22:02:24 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Studentinfo {
     public static void main(String[] args){
-        System.out.println("Alexey Fedoseev");
+        System.out.println("Alexey Fedoseev ");
         System.out.println("15.03.1995");
     }
 }
