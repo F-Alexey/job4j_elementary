602c2e33df18c440f74b1e4bd08bba70a1f9f947
F-Alexey <f.alexey.workspace@gmail.com>
2023-10-29 23:42:56 +0300

    Simple calc, call from Main

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -27,7 +27,7 @@ public class Calculator {
             }
         System.out.println(result);
     }
-    public static void main(String[] args) {
+    public static void main() {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число а ");
         int a = in.nextInt();
--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,9 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("привет");
+        System.out.println("привет, сейчас мы запустим калькулятор из  main файла с вводом данных");
+        Calculator.main();
+        System.out.println("А сейчас запустим его же но с вводом изнутри");
+        Calculator.autoCalc(1,3,"+");
     }
 }
