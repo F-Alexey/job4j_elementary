67c3434b891e80229862ee229203c77cf0b69ad8
F-Alexey <f.alexey.workspace@gmail.com>
2023-11-05 11:59:11 +0300

    Hello World

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,11 +2,6 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("привет, сейчас мы запустим калькулятор из  main файла с вводом данных");
-        Calculator.main();
-        System.out.println("А сейчас запустим его же но с вводом изнутри");
-        Calculator.autoCalc(1, 3, "+");
-        int twise = 4;
-        System.out.println("квадрат 4х равен" + Math.pow(twise, 2));
+        System.out.println("Hello world");
     }
 }
--- src/main/java/ru/job4j/Studentinfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class Studentinfo {
-    public static void main(String[] args) {
-        System.out.println("Alexey Fedoseev ");
-        System.out.println("15.03.1995");
-    }
-}
