7891f09da7c07c54e28a2376a4b92483865af4c6
F-Alexey <f.alexey.workspace@gmail.com>
2023-11-24 18:49:20 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+        public static void main(String[] args) {
+            String idea = "I like Java!";
+            int year = 2023;
+            System.out.println(idea);
+            idea += "But i am newbie.";
+            idea += year;
+            System.out.println(idea);
+        }
+}
