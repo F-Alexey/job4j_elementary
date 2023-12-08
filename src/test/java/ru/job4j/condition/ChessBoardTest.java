62b3b1f9f95937a66e32b6ca8c8bc8bf06b513f5
F-Alexey <f.alexey.workspace@gmail.com>
2023-12-08 21:04:46 +0300

    4.3.6. Math.abs [#373616 #465195]

--- src/test/java/ru/job4j/condition/ChessBoardTest.java
+++ src/test/java/ru/job4j/condition/ChessBoardTest.java
@@ -5,38 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ChessBoardTest {
-    @Test
-    void rookWayIs6() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 6;
-        int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 6;
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    void rookWayIs5() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 5;
-        int y2 = 0;
-        int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 5;
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    void rookWayIs0() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 5;
-        int y2 = 6;
-        int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 0;
-        assertThat(result).isEqualTo(expected);
-    }
 
     @Test
     public void wayIs5() {
