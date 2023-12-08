9952305c9207bb0d2176edcfbcf2ae37428a58b9
F-Alexey <f.alexey.workspace@gmail.com>
2023-12-09 00:38:43 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -3,9 +3,7 @@ package ru.job4j.loop;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
-
 class CounterTest {
-
     @Test
     void when0To5Then15() {
         int a = 0;
@@ -31,5 +29,59 @@ class CounterTest {
         int expected = 55;
         int result = Counter.sum(a, b);
         assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromFourToFifteenThen648() {
+        int start = 4;
+        int finish = 50;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 648;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromOneToOneThenZero() {
+        int start = 1;
+        int finish = 1;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromTwoToTwoThenTwo() {
+        int start = 2;
+        int finish = 2;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromFifteenToZeroThenZero() {
+        int start = 50;
+        int finish = 0;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromMinusFourToSixThenSix() {
+        int start = -4;
+        int finish = 6;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
     }
 }
