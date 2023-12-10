9ac7d4e63b8e25bd9386a44298c59dee5d974867
F-Alexey <f.alexey.workspace@gmail.com>
2023-12-10 15:41:57 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,19 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Alexey";
+        names[1] = "Ksenia";
+        names[2] = "Ludmila";
+        names[3] = "Konstantin";
 
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
     }
 }
