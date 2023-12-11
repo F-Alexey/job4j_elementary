c1ccfe16d77325f988f9eb321a4b54121ce6b0d9
F-Alexey <f.alexey.workspace@gmail.com>
2023-12-11 14:23:32 +0300

    6.1. Классический поиск перебором. [#33489 #465687]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -17,6 +17,7 @@ public class FindLoop {
         for (int i = 0; i < data.length; i++) {
             if (data[i] == element) {
                 result = i;
+                break;
             }
         }
         return result;
